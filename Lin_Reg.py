d31094bb3ae16c9cfdd1a045b69a56d12e717037
Sanjay Sane <sanjay.sane75@gmail.com>
2022-08-21 13:37:51 +0530

    scikit-learn code for comparison
    This file has been mentioned and demonstrated in the YouTube video https://youtu.be/no3mhRsXuwE .

--- Lin_Reg.py
+++ Lin_Reg.py
@@ -1,6 +1,9 @@
 import pandas as pd
-import numpy as np
+
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split 
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
 
 df = pd.read_csv("Boston.csv")
 
@@ -13,25 +16,12 @@ print(regressor.coef_)
 print(regressor.intercept_)
 
 
+############################ Train Test Split ############################
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size = 0.3, 
-                                                    random_state=2021)
+                                                    random_state=2022)
 
-from sklearn.linear_model import LinearRegression
+
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 print(regressor.coef_)
@@ -39,7 +29,23 @@ print(regressor.intercept_)
 
 y_pred = regressor.predict(X_test)
 
-from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+
 print(mean_squared_error(y_test, y_pred))
 print(mean_absolute_error(y_test, y_pred))
 print(r2_score(y_test, y_pred))
+
+## Tree
+
+regressor = DecisionTreeRegressor(random_state=222)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+print(mean_squared_error(y_test, y_pred))
+print(mean_absolute_error(y_test, y_pred))
+print(r2_score(y_test, y_pred))
+
+
+
+
+
