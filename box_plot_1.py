3591ce90d753ae7db90b026146ba03cfedbf237c
Sanjay Sane <sanjay.sane75@gmail.com>
2024-02-21 15:03:23 +0530

    Boxplot in streamlit

---
+++ box_plot_1.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import streamlit as st 
+
+os.chdir(r"C:\Training\Academy\Statistics (Python)\Datasets")
+dia = pd.read_csv("diamonds.csv")
+
+num_columns = list(dia.dtypes[dia.dtypes != object].index)
+cat_columns = [None]+list(dia.dtypes[dia.dtypes == object].index)
+
+st.title("Box Plot")
+col1, col2 = st.columns([0.25, 0.75])
+
+with col1:
+    n_col = st.selectbox('Measure:', 
+                          num_columns, index=0)
+    c_col = st.selectbox('Catogory:', 
+                          cat_columns, index=0)
+with col2:
+    fig, ax = plt.subplots()
+    sns.boxplot(x=c_col, y=n_col, data=dia)
+    st.pyplot(fig)
+
